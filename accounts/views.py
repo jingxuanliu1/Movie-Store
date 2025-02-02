0ae1683566c067b40e483468dee81fd8a3657030
jenhuy <jennyhuynh604@gmail.com>
2025-02-01 22:58:43 -0500

    Resolved conflict in db.sqlite3

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
@@ -8,3 +10,12 @@ def signup(request):
         template_data['form'] = UserCreationForm()
         return render(request, 'accounts/signup.html',
             {'template_data': template_data})
+    elif request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home.index')
+        else:
+            template_data['form'] = form
+            return render(request, 'accounts/signup.html',
+                          {'template_data': template_data})
