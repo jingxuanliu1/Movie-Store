597c09f1772b95610f6c6145d6d11d884abc3a26
jenhuy <jennyhuynh604@gmail.com>
2025-02-07 11:30:39 -0500

    Added movie api and changed index & show html to be compatible

--- movies/views.py
+++ movies/views.py
@@ -1,27 +1,75 @@
+import os
+from dotenv import load_dotenv
+
+import requests
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Movie, Review
 from django.contrib.auth.decorators import login_required
 
+load_dotenv()
+TMDB_API_KEY = os.getenv('TMDB_API_KEY')
+
+
+def get_movies(search_term=None):
+    base_url = "https://api.themoviedb.org/3"
+
+    if search_term:
+        url = f"{base_url}/search/movie?api_key={TMDB_API_KEY}&language=en-US&query={search_term}"
+    else:
+        url = f"{base_url}/movie/popular?api_key={TMDB_API_KEY}&language=en-US&page=1"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+
+        movies = [
+            {
+                'id': movie['id'],
+                'name': movie['title'],
+                'price': 10,
+                'description': movie['overview'],
+                'poster': f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"
+            }
+            for movie in data.get('results', [])
+        ]
+
+        return movies
+    else:
+        return []
+
+def get_movie_details(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={TMDB_API_KEY}&language=en-US"
+    response = requests.get(url)
+    if response.status_code == 200:
+        movie = response.json()
+        return {
+            'id': movie['id'],
+            'name': movie['title'],
+            'description': movie['overview'],
+            'poster': f"https://image.tmdb.org/t/p/w500{movie['poster_path']}"
+        }
+    return None
+
 def index(request):
     search_term = request.GET.get('search')
-    if search_term:
-        movies = Movie.objects.filter(name__icontains=search_term)
-    else:
-        movies = Movie.objects.all()
 
-    template_data = {}
-    template_data['title'] = 'Movies'
-    template_data['movies'] = movies
-    return render(request, 'movies/index.html', {'template_data': template_data})
+    template_data = {
+        'title': 'Movies',
+        'movies': get_movies(search_term),
+    }
+    return render(request, 'movies/index.html', {'template_data':template_data})
 
 def show(request, id):
-    movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie)
+    movie = get_movie_details(id)
+    if not movie:
+        return render(request, 'movies/show.html', {'template_data': template_data})
+    reviews = Review.objects.filter(movie_id=id)
 
-    template_data = {}
-    template_data['title'] = movie.name
-    template_data['movie'] = movie
-    template_data['reviews'] = reviews
+    template_data = {
+        'title': movie['name'],
+        'movie': movie,
+        'reviews': reviews,
+    }
     return render(request, 'movies/show.html', {'template_data': template_data})
 
 @login_required
